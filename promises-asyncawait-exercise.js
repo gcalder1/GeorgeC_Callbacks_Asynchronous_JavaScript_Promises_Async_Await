089fea7755ef58b963e4324d03c44d06c83b940b
unknown <george.calderon2000@gmail.com>
2025-04-30 01:49:03 -0400

    Finished exercise
    Notes on my thought process to completion is missing

---
+++ promises-asyncawait-exercise.js
@@ -0,0 +1,34 @@
+function addStudentToClass(nameStudent, ageStudent){
+    return new Promise((accStudentToClass, rejStudentFromClass) => {
+        if (ageStudent < 18){
+            rejStudentFromClass(`${nameStudent} is too young for this class`);
+        } else {
+            setTimeout(() => {
+                accStudentToClass(`${nameStudent} added to the class`);
+            }, Math.floor(Math.random() * 2001) + 1000);
+        }
+    }) 
+} 
+
+async function registerStudent(student){
+    try {
+        const msg = await addStudentToClass(student.age);
+        console.log(msg);
+    } catch (err) {
+        console.log(err);
+    }
+}
+    
+
+// This code should sit at the bottom of your
+// solution
+// Plan accordingly
+const students = [
+    { name: "Alice", age: 20 },
+    { name: "Bob", age: 17 },
+    { name: "Charlie", age: 19 },
+];
+
+for (const student of students) {
+    registerStudent(student);
+}
